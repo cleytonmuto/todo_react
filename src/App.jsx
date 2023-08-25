f6940f3d29aedd9747a52dfcee7820e78b1443a9
cleytonmuto <cleytonmuto@gmail.com>
2023-08-24 21:29:49 -0300

    feat: completed the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { useState } from 'react';
 
 import Todo from './components/Todo';
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
+import Filter from './components/Filter';
 
 import './App.css';
+
 
 
 function App() {
@@ -28,15 +31,57 @@ function App() {
     }
   ]);
 
+  const [search, setSearch] = useState('');
+  const [filter, setFilter] = useState('All');
+  const [sort, setSort] = useState('Asc');
+
+  const addTodo = (text, category) => {
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false
+    }];
+    setTodos(newTodos);
+  }
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) => {
+      return todo.id !== id ? todo : null;
+    });
+    setTodos(filteredTodos);
+  }
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) => {
+      return todo.id === id ? todo.isCompleted = !todo.isCompleted : todo;
+    })
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
-        <h1>Lista de Tarefas</h1>
-        <div className="todo-list">
-          {todos.map((todo) => (
-            <Todo todo={todo} />
+      <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
+      <div className="todo-list">
+        {todos
+          .filter(
+            (todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted
+          )
+          .filter(
+            (todo) => todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort(
+            (a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text)
+          )
+          .map((todo) => (
+            <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
           ))}
-        </div>
-        <TodoForm />
+      </div>
+      <TodoForm addTodo={addTodo} />
     </div>
   );
 }
